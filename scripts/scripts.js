01546811bb80625d80491327f7bd184364e41dcf
Sherwin <74441382+sherwin-leung@users.noreply.github.com>
2024-10-01 21:36:48 -0700

    Initial Commit
    The starting point

---
+++ scripts/scripts.js
@@ -0,0 +1,7 @@
+function loadTest() {
+     document.getElementById("load").style.display = "block";
+}
+
+function createTest() {
+     document.getElementById("create").style.display = "block";
+}
